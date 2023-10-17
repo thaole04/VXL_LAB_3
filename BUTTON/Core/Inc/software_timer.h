a2844b662ee5dadc3df8e8243f3b44f00765d257
thaole04 <thao.le14758@hcmut.edu.vn>
2023-10-17 22:06:11 +0700

    first commit

---
+++ BUTTON/Core/Inc/software_timer.h
@@ -0,0 +1,19 @@
+/*
+ * software_timer.h
+ *
+ *  Created on: Oct 17, 2023
+ *      Author: thao2
+ */
+
+#ifndef INC_SOFTWARE_TIMER_H_
+#define INC_SOFTWARE_TIMER_H_
+
+// variable
+extern int timer0_flag;
+
+// function
+void setTimer0(int duration);
+void timerRun();
+
+
+#endif /* INC_SOFTWARE_TIMER_H_ */
